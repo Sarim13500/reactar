10680908b33ba06535295520d6ed989db94e012f
Mohammad Junaid Ali#EXT# <Mohammad.Ali@triona.no>
2024-02-21 17:23:45 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ARScene from "./components/ARScene";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <ARScene />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ARScene.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useRef } from "react";
+import * as THREE from "three";
+import * as THREEx from "@ar-js-org/ar.js/three.js/build/ar-threex-location-only.js";
+
+const ARScene = () => {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(60, 1.33, 0.1, 10000);
+    const renderer = new THREE.WebGLRenderer({ canvas });
+
+    const arjs = new THREEx.LocationBased(scene, camera);
+    const cam = new THREEx.WebcamRenderer(renderer);
+
+    const geom = new THREE.BoxGeometry(20, 20, 20);
+    const mtl = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const box = new THREE.Mesh(geom, mtl);
+    arjs.add(box, -0.72, 51.051);
+
+    arjs.fakeGps(-0.72, 51.05);
+
+    function render() {
+      if (
+        canvas.width != canvas.clientWidth ||
+        canvas.height != canvas.clientHeight
+      ) {
+        renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
+        const aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.aspect = aspect;
+        camera.updateProjectionMatrix();
+      }
+      cam.update();
+      renderer.render(scene, camera);
+      requestAnimationFrame(render);
+    }
+
+    render();
+
+    return () => {
+      // Clean up code here (if needed)
+    };
+  }, []);
+
+  return (
+    <canvas
+      ref={canvasRef}
+      style={{ backgroundColor: "black", width: "100%", height: "100%" }}
+    />
+  );
+};
+
+export default ARScene;

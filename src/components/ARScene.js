53b29a4258e0b3c2a6c9bff4448da2fafc3d3ea4
Mohammad Junaid Ali#EXT# <Mohammad.Ali@triona.no>
2024-03-06 10:36:05 +0100

    helu

--- src/components/ARScene.js
+++ src/components/ARScene.js
@@ -1,51 +1,44 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import * as THREE from "three";
 import * as THREEx from "@ar-js-org/ar.js/three.js/build/ar-threex-location-only.js";
 
 const ARScene = () => {
   const canvasRef = useRef(null);
+  const [fetched, setFetched] = useState(false);
 
   useEffect(() => {
-    axios
-      .get(
-        "https://augmented-api.azurewebsites.net/manholes/latlong?latitude=59.907809&longitude=10.758035"
-      )
-      .then((response) => {
-        // Extracting and logging wkt values
-        response.data.forEach((manhole) => {
-          console.log(manhole);
-          console.log("Extracting wkt...");
-          console.log(manhole.wkt);
-          console.log("Extracting long lat...");
-          console.log(manhole.long);
-          console.log(manhole.lat);
+    const arjs = new THREEx.LocationBased();
+    const geom = new THREE.BoxGeometry(1, 1, 1);
+    const mtl = new THREE.MeshBasicMaterial({ color: 0x8a2be2 });
 
-          // Create a box for each manhole
-          const geom = new THREE.BoxGeometry(3, 3, 3);
-          const mtl = new THREE.MeshBasicMaterial({ color: 0x8a2be2 });
-          const box = new THREE.Mesh(geom, mtl);
+    arjs.startGps();
 
-          // Add the box to the AR scene at the manhole's coordinates
-          arjs.add(box, manhole.long, manhole.lat);
-        });
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
+    arjs.on("gpsupdate", async (pos) => {
+      if (!fetched) {
+        try {
+          const response = await axios.get(
+            `https://augmented-api.azurewebsites.net/manholes/latlong?latitude=${pos.coords.latitude}&longitude=${pos.coords.longitude}`
+          );
+          response.data.forEach((manhole) => {
+            const box = new THREE.Mesh(geom, mtl);
+            arjs.add(box, manhole.long, manhole.lat);
+          });
+          setFetched(true);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      }
+    });
 
     const canvas = canvasRef.current;
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(60, 1.33, 0.1, 10000);
     const renderer = new THREE.WebGLRenderer({ canvas });
-    const arjs = new THREEx.LocationBased(scene, camera);
     const cam = new THREEx.WebcamRenderer(renderer);
-
     const deviceOrientationControls = new THREEx.DeviceOrientationControls(
       camera
     );
-
-    arjs.startGps();
 
     // Render function
     function render() {

fd739183825ef4edae3e879164d39cc8074d5075
Mohammad Junaid Ali#EXT# <Mohammad.Ali@triona.no>
2024-03-07 09:38:15 +0100

    po

--- src/components/ARScene.js
+++ src/components/ARScene.js
@@ -27,7 +27,6 @@ const ARScene = () => {
             console.log("Extracting wkt...");
             console.log(manhole.wkt);
             console.log("Extracting long lat...");
-            console.log("Extracting long lat...");
             console.log(manhole.long);
             console.log(manhole.lat);
 

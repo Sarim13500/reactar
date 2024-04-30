36b63abd3a752470c0041e47183ac4cf23737bd5
Abdul Moiz Ehsan#EXT# <Abdul.Ehsan@triona.no>
2024-04-30 13:09:20 +0200

    bedre

--- src/components/ARScene.js
+++ src/components/ARScene.js
@@ -78,15 +78,6 @@ const ARScene = () => {
         const box = new THREE.Mesh(geom, mtl);
         arjs.add(box, model.long, model.lat);
       });
-
-      fillManholeModelsWithData({
-        manholeModels: withinRadius,
-        labels: [],
-        scene: scene,
-        geom: geom,
-        mtl: mtl,
-      });
-
       localStorage.setItem("manholeData", JSON.stringify(manholeModels));
     });
 
@@ -157,8 +148,7 @@ const calculateDistance = (lat1, lon1, lat2, lon2) => {
   return d;
 };
 
-const fillManholeModelsWithData = ({ manholeModels, labels, scene, geom,
-  mtl, }) => {
+const fillManholeModelsWithData = ({ manholeModels, labels, scene }) => {
   let boxes;
 
   const boxMesh = new THREE.Mesh(geom, mtl);
